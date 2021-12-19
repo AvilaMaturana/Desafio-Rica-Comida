aaa25706d2dd77223f6fcc3c8fcc9289007c63a6
Diego Ávila Maturana <94457305+AvilaMaturana@users.noreply.github.com>
2021-12-19 11:12:36 -0300

    First Commit Desafio Rica Comida

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$(document).ready(function () {
+    $("#enviarCorreo").click(function () {
+        alert("Correo enviado correctamente")
+
+    });
+});
+
+$(document).ready(function () {
+    $("#ingredientes").on("dblclick",function () {
+        $(this).css({
+            "color": "red",
+            
+        });
+    });
+});
+
+$(document).ready(function () {
+
+    $("#ingredientes").on("dblclick",function () {
+        $(this).css({
+            "color": "red",            
+        });               
+    }); 
+
+    $("#ingredientes").click(function () {
+        $(this).css({
+            "color": "#373A3C", 
+        });
+    });
+      
+
+    $("#preparacion").on("dblclick",function () {
+        $(this).css({
+            "color": "red",            
+        });       
+    }); 
+    
+    $("#preparacion").click(function () {
+        $(this).css({
+            "color": "#373A3C",
+        });
+    });   
+    
+});
+
+
+
+
+
+$(document).ready(function () {
+    $("#cardUno").on("click",function () {
+        $("#textUno").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#cardDos").on("click",function () {
+        $("#textDos").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#cardTres").on("click",function () {
+        $("#textTres").toggle();
+    });
+});
